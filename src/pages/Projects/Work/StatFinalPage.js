39266fa78d3b30c56b5aed160532e5fc7f7f0543
Bryan Ge <bryange3@gmail.com>
2021-12-10 22:20:19 -0600

    Update final project image

--- src/pages/Projects/Work/StatFinalPage.js
+++ src/pages/Projects/Work/StatFinalPage.js
@@ -3,21 +3,19 @@ import Content from '../../../components/Content.js';
 import ProjectTitle from '../../../components/ProjectTitle.js';
 import FadeIn from 'react-fade-in';
 
+import stat107finalproject from '../../../assets/project/stat107final/stat107finalproject.png';
+
 function StatFinalPage(props) {
     window.scrollTo(0, 0);
-    const width = 8;
+    const width = 10;
     return (
         <FadeIn>
             <ProjectTitle info={props.info} width={width} />
             <Content width={width}>
                 <body>
-                    <hr class="rounded" />
-                    <iframe
-                        title="STAT 107 Final Project Jupyter Notebook"
-                        className="full-width"
-                        src="src/assets/project/stat107final/STAT107FinalProject.pdf"
-                        frameborder="0"
-                    ></iframe>
+                    <hr class="rounded med-text" />
+                    
+                    <img src={stat107finalproject} alt="Project PDF" className="project-img" />
                 </body>
             </Content>
         </FadeIn>
