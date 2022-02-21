e584c0464bf780d3271cf92206c2eb3898c5d698
Bryan Ge <bryange3@gmail.com>
2022-02-21 10:25:51 -0600

    Update resume

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
             <p className="footer-title">Thanks for coming, let's chat!</p>
             <ul>
                 <li><a href="mailto:bryange3@gmail.com" target="_blank" rel="noreferrer" className="social" alt="Email">Email</a></li>
-                <li><a href="https://bryange.read.cv" target="_blank" rel="noreferrer" className="social" alt="GitHub">CV</a></li>
+                <li><a href="https://drive.google.com/file/d/1e-ACTyMqLo_neKqfUbgBYhQwGw_DjD7e/view?usp=sharing" target="_blank" rel="noreferrer" className="social" alt="Resume">Resume</a></li>
                 <li><a href="https://github.com/bryange3" target="_blank" rel="noreferrer" className="social" alt="GitHub">GitHub</a></li>
                 <li><a href="https://instagram.com/omgebryan" target="_blank" rel="noreferrer" className="social" alt="Instagram">Instagram</a></li>
                 <li><a href="https://open.spotify.com/user/omgebryan?si=AzsYGAQnQmS8m6Jlg-cUMw" target="_blank" rel="noreferrer" className="social" alt="Spotify">Spotify</a></li>
